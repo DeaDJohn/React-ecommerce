b33b05b23a4c59c14988d334bb6cab975c5e0946
juanjo <juanjo@tooltyp.com>
2019-07-04 19:09:42 +0200

    Se ha quedado un archivo sin subir :)

--- src/reducers/productListReducer.js
+++ src/reducers/productListReducer.js
@@ -10,5 +10,48 @@ import {
 import initialState from './initialState';
 
 export default function productListReducer (state = initialState.productList, action){
-
+    switch( action.type){
+        case FETCH_PRODUCTS_INIT:
+            return {
+                ...state,
+                error: null,
+                loading: true
+            };
+        case FETCH_PRODUCTS_FAILURE:
+            return{
+                ...state,
+                products: [],
+                error: action.payload,
+                loading: false
+            }
+        case FETCH_PRODUCTS_SUCCESS:
+            return {
+                ...state,
+                products: action.payload,
+                error: null,
+                loading: false
+            }
+        case SAVE_PRODUCT_INIT:
+            return {
+                ...state,
+                error: null,
+                loading: true
+            };
+        case SAVE_PRODUCT_FAILURE:
+            return{
+                ...state,
+                products: [],
+                error: action.payload,
+                loading: false
+            }
+        case SAVE_PRODUCT_SUCCESS:
+            return {
+                ...state,
+                products: [...state.products, action.payload],
+                error: null,
+                loading: false
+            }
+        default:
+            return state;
+    }
 }
